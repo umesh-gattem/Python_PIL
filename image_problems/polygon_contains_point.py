532bccf8a945a6160581e2649c2c666e2ca8b029
umesh <umesh.gattem@gmail.com>
2016-10-27 10:26:15 +0530

    changed polygon_contains_point problem

--- image_problems/polygon_contains_point.py
+++ image_problems/polygon_contains_point.py
@@ -7,12 +7,13 @@ inside any of the text areas or not. Display the result to console.
 
 from PIL import ImageDraw, Image
 import json
+import random
 from collections import OrderedDict
 from shapely import geometry as gm
 
-
 with open("../json/first_image.json") as json_file:
     im = Image.open('../images/first_image.jpg')
+    width, height = im.size
     draw = ImageDraw.Draw(im)
     json_data = json.load(json_file, object_pairs_hook=OrderedDict)
     text_areas = []
@@ -22,10 +23,14 @@ with open("../json/first_image.json") as json_file:
             for each_co_ordinate in co_ordinates:
                 res.append(tuple(each_co_ordinate))
             text_areas.append(res)
-    for text_area in text_areas:
-        if len(text_area) == 4:
-            if gm.Polygon(text_area).contains(gm.Point(91.5, 153)):
-                print("true")
-                exit()
-            else:
-                print('false')
+    points = [(random.randint(0, width), random.randint(0, height)) for k in range(10)]
+    print("Co_ordinates", "  " 'Falling in text areas')
+    for point in points:
+        flag = 0
+        for text_area in text_areas:
+            if len(text_area) == 4:
+                if gm.Polygon(text_area).contains(gm.Point(point)):
+                    print(point, "    ", 'True')
+                    flag = 1
+        if flag == 0:
+            print(point, "   ", 'False')
